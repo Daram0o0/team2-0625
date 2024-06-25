5a3ece755ee6ea2f46bd977728438fd815599743
Daram0o0 <c30728535@gmail.com>
2024-06-25 16:39:41 +0900

    Feat: add handleDivide

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import "./App.css";
 
 function App() {
   const [num, setNum] = useState(0);
+
+  const handleDivide = () => {
+    const newNum = num / 2;
+    setNum(newNum);
+  };
+
   return (
     <div className="App">
       <div>
@@ -12,7 +18,7 @@ function App() {
         <button>+</button>
         <button>-</button>
         <button>*</button>
-        <button>/</button>
+        <button onClick={handleDivide}>/</button>
       </div>
     </div>
   );
